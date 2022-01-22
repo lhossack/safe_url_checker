7216867d3c7e2b5fae347fec652940862497ba09
lhossack <l.hossack217@gmail.com>
2022-01-22 14:39:15 -0700

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Safe Url Checker
+
+A web service for checking if urls are known to contain malware.
+
+## Installation Quickstart
+
+
+## Deployment Strategies
+
+
+## Release Process
+
+
+## Release History
+Please see CHANGELOG.md
+
+## Contributing
+It is recommended to install dependencies in a virtual environment.
+
+### Using virtual environments
+
+To initialize a virtualenv on a *nix based OS:
+
+```
+$ python3 -m venv .venv
+```
+
+After venv initialization, activate your venv using:
+
+```
+$ source .venv/bin/activate
+```
+
+On Windows, activate venv like this:
+
+```
+% .venv\Scripts\activate.bat
+```
+
+To install required dependencies, run this command:
+
+```
+$ pip install -r requirements.txt
+```
+
+To add additional dependencies, add them to the `setup.py` file and 
+rerun the `pip install -r requirements.txt` command.
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+
+setuptools.setup(
+    name="Safe Url Checker",
+    version="0.0.1",
+    description="Web service for checking URLs for known malware",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Lucas Hossack",
+    # TODO: Update package dir placeholder
+    package_dir={"": "urlchecker"},
+    packages=setuptools.find_packages(where="urlchecker"),
+    install_requires=[],
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+)
