95c9ec809c76a9c26996919c0e991a797f7f2d1c
lhossack <l.hossack217@gmail.com>
2022-01-25 00:05:47 -0700

    Fix broken log levels & separate log config

--- urlchecker/flask_frontend.py
+++ urlchecker/flask_frontend.py
@@ -1,47 +1,9 @@
+import log_config
 import config_reader
 import os
 
 import werkzeug
 from flask import Flask, request, jsonify
-from logging.config import dictConfig
-
-if "URLINFO_LOGLEVEL" in os.environ and os.environ["URLINFO_LOGLEVEL"] in [
-    "INFO",
-    "DEBUG",
-    "WARN",
-    "ERROR",
-    "CRITICAL",
-]:
-    LOGLEVEL = os.environ["URLINFO_LOGLEVEL"]
-else:
-    LOGLEVEL = "WARN"
-
-dictConfig(
-    {
-        "version": 1,
-        "formatters": {
-            "default": {
-                "format": "%(asctime)s %(levelname)s %(module)s: %(message)s",
-            }
-        },
-        "handlers": {
-            "wsgi": {
-                "class": "logging.StreamHandler",
-                "stream": "ext://flask.logging.wsgi_errors_stream",
-                "formatter": "default",
-            }
-        },
-        "loggers": {
-            "urlchecker": {"handlers": ["wsgi"], "level": LOGLEVEL, "propagate": False},
-            "dbm_adaptor": {
-                "handlers": ["wsgi"],
-                "level": LOGLEVEL,
-                "propagate": False,
-            },
-        },
-        "root": {"level": LOGLEVEL, "handlers": ["wsgi"]},
-    }
-)
 
 
 def create_app(test_config=None):
@@ -50,11 +12,12 @@ def create_app(test_config=None):
     Defines route handlers, which pass off control almost immediately
     once done with flask-specific code to handle IO
     """
-    app = Flask(__name__)
 
     urlchecker_cfg = config_reader.ConfigReader()
     urlchecker_cfg.configure()
     urlchecker = urlchecker_cfg.urlchecker
+
+    app = Flask(__name__)
 
     try:
         os.makedirs(app.instance_path)
@@ -66,25 +29,29 @@ def create_app(test_config=None):
     @app.errorhandler(Exception)
     def handle_error(e):
         """Return error for anything other than urlinfo/1/ route, or on any unhandled exception"""
-        app.logger.exception(f"Application exception: {e.code}", exc_info=e)
         try:
+            app.logger.info(f"Client or server error {e.code}", exc_info=e)
             return (jsonify({"status": "unknown", "reason": "invalid request"}), e.code)
         except:
+            app.logger.exception(f"Application exception: ", exc_info=e)
             return (jsonify({"status": "unknown", "reason": "server error"}), 500)
 
     @app.route("/urlinfo/1/<path:hostpath>", methods=["GET"])
     def urlinfo(hostpath):
         """The main route for urlchecker interface"""
+        app.logger.debug(f"Request: {hostpath}")
         urldata = request.full_path[len("/urlinfo/1/") :]
 
         # Werkzeug appends a '?' when there are no query params
         if urldata.endswith("?"):
             urldata = urldata[:-1]
 
+        app.logger.debug(f"Checking: '{werkzeug.urls.iri_to_uri(urldata)}'")
         status, reason = urlchecker.check_url_has_malware(
             werkzeug.urls.iri_to_uri(urldata)
         )
-
-        return jsonify({"status": status, "reason": reason})
+        response = jsonify({"status": status, "reason": reason})
+        app.logger.debug(f"Response: {response}")
+        return response
 
     return app
---
+++ urlchecker/log_config.py
@@ -0,0 +1,50 @@
+from logging.config import dictConfig
+import os
+
+if "URLINFO_LOGLEVEL" in os.environ and os.environ["URLINFO_LOGLEVEL"] in [
+    "DEBUG",
+    "INFO",
+    "WARNING",
+    "ERROR",
+    "CRITICAL",
+]:
+    LOGLEVEL = os.environ["URLINFO_LOGLEVEL"]
+else:
+    LOGLEVEL = "WARNING"
+
+dictConfig(
+    {
+        "version": 1,
+        "formatters": {
+            "default": {
+                "format": "%(asctime)s %(levelname)s %(module)s: %(message)s",
+            }
+        },
+        "handlers": {
+            "wsgi": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://flask.logging.wsgi_errors_stream",
+                "formatter": "default",
+            },
+            "default": {
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stderr",
+                "formatter": "default",
+            },
+        },
+        "loggers": {
+            "urlchecker": {
+                "handlers": ["default"],
+                "level": LOGLEVEL,
+                "propagate": False,
+            },
+            "dbm_adaptor": {
+                "handlers": ["default"],
+                "level": LOGLEVEL,
+                "propagate": False,
+            },
+        },
+        "root": {"level": LOGLEVEL, "handlers": ["wsgi"]},
+    }
+)
+print(f" ** Log level: {LOGLEVEL}")
